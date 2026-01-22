42ff34c2f27f09ab88df464545c8ebe07489164c
Patniss <vanina.lancelle@gmail.com>
2026-01-22 09:34:14 +0100

    supabase connexion

---
+++ js/supabase.js
@@ -0,0 +1,8 @@
+<script type="module">
+  import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
+
+  const supabaseUrl = "https://clffgsknhzqlywvcimcj.supabase.co";
+  const supabaseKey = "sb_publishable_pga62VTZSN5TWSTXl-De0w_Vqe7APE9";
+
+  export const supabase = createClient(supabaseUrl, supabaseKey);
+</script>
